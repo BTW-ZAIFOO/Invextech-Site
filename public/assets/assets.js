6d64bfa5fcd3ac4a05dac8620c22cda6808c5d53
unknown <{huzaifazaifi25@gmail.com}>
2024-12-24 11:26:13 +0500

    renaming images

--- public/assets/assets.js
+++ public/assets/assets.js
@@ -1,29 +1,29 @@
-import logo from './logo.svg'
-import logo_dark from './logo_dark.svg'
-import cross_icon from './cross_icon.svg'
-import menu_icon from './menu_icon.svg'
-import star_icon from './star_icon.svg'
-import brand_img from './brand_img.png'
-import project_img_1 from './project_img_1.jpg'
-import project_img_2 from './project_img_2.jpg'
-import project_img_3 from './project_img_3.jpg'
-import project_img_4 from './project_img_4.jpg'
-import invextech from './invextech.svg'
-import mono from './mono.svg'
-import logo_invextech from './logo-invextech.png'
-import logo_preview from './logo_preview.png'
-import whatsApp from './WhatsApp.jpg'
-import development_img from './development_img.webp'
-import download from './download.jpeg'
-import images from './images.jpeg'
-import images_1 from './images_1.jpeg'
-import images_2 from './images_2.jpeg'
-import images_3 from './images_3.png'
-import images_4 from './images_4.jpeg'
-import images_5 from './images_5.png'
-import images_6 from './images_6.jpeg'
-import ceo from './Ceo_img.jpeg'
-import coo from './Coo_img.jpeg'
+import logo from './logo.svg';
+import logo_dark from './logo_dark.svg';
+import cross_icon from './cross_icon.svg';
+import menu_icon from './menu_icon.svg';
+import star_icon from './star_icon.svg';
+import brand_img from './brand_img.png';
+import project_img_1 from './project_img_1.jpg';
+import project_img_2 from './project_img_2.jpg';
+import project_img_3 from './project_img_3.jpg';
+import project_img_4 from './project_img_4.jpg';
+import invextech from './invextech.svg';
+import mono from './mono.svg';
+import logo_invextech from './logo-invextech.png';
+import logo_preview from './logo_preview.png';
+import whatsapp from './whatsapp.jpg'; // Renamed to lowercase for consistency
+import development_img from './development_img.webp';
+import download from './download.jpeg';
+import images from './images.jpeg';
+import images_1 from './images_1.jpeg';
+import images_2 from './images_2.jpeg';
+import images_3 from './images_3.png';
+import images_4 from './images_4.jpeg';
+import images_5 from './images_5.png';
+import images_6 from './images_6.jpeg';
+import ceo from './ceo_img.jpeg'; // Renamed to lowercase for consistency
+import coo from './coo_img.jpeg'; // Renamed to lowercase for consistency
 
 export const assets = {
     logo,
@@ -40,7 +40,7 @@ export const assets = {
     mono,
     logo_invextech,
     logo_preview,
-    whatsApp,
+    whatsapp, // Updated naming
     development_img,
     download,
     images,
@@ -50,7 +50,8 @@ export const assets = {
     images_4,
     images_5,
     images_6,
-    ceo,
-    coo,
-}
-export default assets ;
+    ceo, // Updated naming
+    coo, // Updated naming
+};
+
+export default assets;
