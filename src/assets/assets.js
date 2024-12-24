51c2626c3dc7c950fe2e108c4d231cd9b82e988f
unknown <{huzaifazaifi25@gmail.com}>
2024-12-24 11:45:27 +0500

    renaming images

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,61 +1,40 @@
-import logo from '../assets/logo.svg'
-import logo_dark from '../assets/logo_dark.svg'
-import cross_icon from '../assets/cross_icon.svg'
-import menu_icon from '../assets/menu_icon.svg'
-import star_icon from '../assets/star_icon.svg'
-import left_arrow from '../assets/left_arrow.svg'
-import right_arrow from '../assets/right_arrow.svg'
-import header_img from '../assets/header_img.png'
-import brand_img from '../assets/brand_img.png'
-import project_img_1 from '../assets/project_img_1.jpg'
-import project_img_2 from '../assets/project_img_2.jpg'
-import project_img_3 from '../assets/project_img_3.jpg'
-import project_img_4 from '../assets/project_img_4.jpg'
-import project_img_5 from '../assets/project_img_5.jpg'
-import project_img_6 from '../assets/project_img_6.jpg'
-import profile_img_1 from '../assets/profile_img_1.png'
-import profile_img_2 from '../assets/profile_img_2.png'
-import profile_img_3 from '../assets/profile_img_3.png'
-// import Invextech from '../assets/Invextech.svg'
-import invextech from '../assets/invextech.svg'
-import mono from '../assets/mono.svg'
-import logo_invextech from './logo-invextech.png'
-import code_img from '../assets/code_img.jpg'
-import logo_preview from '../assets/logo_preview.png'
-import WhatsApp from '../assets/WhatsApp.jpg'
-import development_img from '../assets/development_img.webp'
-import download from '../assets/download.jpeg'
-import images from '../assets/images.jpeg'
-import images_1 from '../assets/images_1.jpeg'
-import images_2 from '../assets/images_2.jpeg'
-import images_3 from '../assets/images_3.png'
-import images_4 from '../assets/images_4.jpeg'
-import images_5 from '../assets/images_5.png'
-import images_6 from '../assets/images_6.jpeg'
-import CEO from '../assets/CEO.jpeg'
-import COO from '../assets/COO.jpeg'
+import logo from '../assets/logo.svg';
+import logo_dark from '../assets/logo_dark.svg';
+import cross_icon from '../assets/cross_icon.svg';
+import menu_icon from '../assets/menu_icon.svg';
+import project_img_1 from '../assets/project_img_1.jpg';
+import project_img_2 from '../assets/project_img_2.jpg';
+import project_img_3 from '../assets/project_img_3.jpg';
+import project_img_4 from '../assets/project_img_4.jpg';
+import invextech from '../assets/invextech.svg';
+import mono from '../assets/mono.svg';
+import logo_preview from '../assets/logo_preview.png';
+import whatsapp from '../assets/whatsapp.jpg';
+import development_img from '../assets/development_img.webp';
+import download from '../assets/download.jpeg';
+import images from '../assets/images.jpeg';
+import images_1 from '../assets/images_1.jpeg';
+import images_2 from '../assets/images_2.jpeg';
+import images_3 from '../assets/images_3.png';
+import images_4 from '../assets/images_4.jpeg';
+import images_5 from '../assets/images_5.png';
+import images_6 from '../assets/images_6.jpeg';
+import ceo from '../assets/ceo_img.jpeg'; 
+import coo from '../assets/coo_img.jpeg'; 
 
 export const assets = {
     logo,
     logo_dark,
     cross_icon,
     menu_icon,
-    star_icon,
-    header_img,
-    brand_img,
     project_img_1,
     project_img_2,
     project_img_3,
     project_img_4,
-    left_arrow,
-    right_arrow,
-    // Invextech,
     invextech,
     mono,
-    logo_invextech,
-    code_img,
     logo_preview,
-    WhatsApp,
+    whatsapp,
     development_img,
     download,
     images,
@@ -65,75 +44,8 @@ export const assets = {
     images_4,
     images_5,
     images_6,
-    CEO,
-    COO
-}
+    ceo,
+    coo,
+};
 
-export const projectsData = [
-    {
-      title: "Skyline Haven",
-      price: "$2,50,000",
-      location: "California",
-      image: project_img_1
-    },
-    {
-      title: "Vista Verde",
-      price: "$2,50,000",
-      location: "San Francisco",
-      image: project_img_2
-    },
-    {
-      title: "Serenity Suites",
-      price: "$2,50,000",
-      location: "Chicago",
-      image: project_img_3
-    },
-    {
-      title: "Central Square",
-      price: "$2,50,000",
-      location: "Los Angeles",
-      image: project_img_4
-    },
-    {
-      title: "Vista Verde",
-      price: "$2,50,000",
-      location: "San Francisco",
-      image: project_img_5
-    },
-    {
-      title: "Serenity Suites",
-      price: "$2,50,000",
-      location: "Chicago",
-      image: project_img_6
-    },
-    
-  ];
-
-  export const testimonialsData = [
-    {
-        name: "Donald Jackman",
-        title: "Marketing Manager",
-        image: profile_img_1,
-        alt: "Portrait of Donald Jackman",
-        rating: 5,
-        text: "From the very first meeting, they understood my vision and helped me find the perfect property. Their attention to detail and commitment to client satisfaction is unmatched."
-    },
-    {
-        name: "Richard Nelson",
-        title: "UI/UX Designer",
-        image: profile_img_2,
-        alt: "Portrait of Richard Nelson",
-        rating: 4,
-        text: "From the very first meeting, they understood my vision and helped me find the perfect property. Their attention to detail and commitment to client satisfaction is unmatched."
-    },
-    {
-        name: "James Washington",
-        title: "Co-Founder",
-        image: profile_img_3,
-        alt: "Portrait of James Washington",
-        rating: 5,
-        text: "From the very first meeting, they understood my vision and helped me find the perfect property. Their attention to detail and commitment to client satisfaction is unmatched."
-    }
-];
-
-export default assets
+export default assets;
