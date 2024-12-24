1815480b027d876d50617372d0e459fc6c0ad5e8
unknown <{huzaifazaifi25@gmail.com}>
2024-12-24 12:49:34 +0500

    change images path

--- src/Components/Company.jsx
+++ src/Components/Company.jsx
@@ -5,7 +5,7 @@ const Company = () => {
     {
       name: "Mr. Abu-Bakar Waqas",
       role: "CEO",
-      image: "/src/assets/Ceo_img.jpeg",
+      image: "https://www.linkedin.com/in/abubakar-waqas-a25182199/overlay/photo/",
       bio: "A visionary leader driving innovation and growth at our company.",
       socials: {
         linkedin: "https://www.linkedin.com/in/abubakar-waqas-a25182199",
@@ -14,7 +14,7 @@ const Company = () => {
     {
       name: "Mr. Muhsan Sattar",
       role: "COO",
-      image: "/src/assets/Coo_img.jpeg",
+      image: "https://www.linkedin.com/in/muhsan-sattar-b7b0801a4/overlay/photo/",
       bio: "An operational expert ensuring seamless execution of our strategies.",
       socials: {
         linkedin: "https://www.linkedin.com/in/muhsan-sattar-b7b0801a4",
