46250269feb95734e524c60f7c4a1ca544e5b2d5
unknown <{huzaifazaifi25@gmail.com}>
2024-12-24 12:29:23 +0500

    comment images

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -5,7 +5,7 @@ import menu_icon from '../assets/menu_icon.svg';
 import invextech from '../assets/invextech.svg';
 import mono from '../assets/mono.svg';
 import logo_preview from '../assets/logo_preview.png';
-import whatsApp from '../assets/whatsApp.jpg';
+// import whatsApp from '../assets/whatsApp.jpg';
 import development_img from '../assets/development_img.webp';
 import download from '../assets/download.jpeg';
 import images from '../assets/images.jpeg';
@@ -26,7 +26,7 @@ export const assets = {
     invextech,
     mono,
     logo_preview,
-    whatsApp,
+    // whatsApp,
     development_img,
     download,
     images,
