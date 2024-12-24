26a556d678cbc4fe8b81310536919ced30486a37
unknown <{huzaifazaifi25@gmail.com}>
2024-12-24 11:23:39 +0500

    removing images

--- public/assets/assets.js
+++ public/assets/assets.js
@@ -3,26 +3,16 @@ import logo_dark from './logo_dark.svg'
 import cross_icon from './cross_icon.svg'
 import menu_icon from './menu_icon.svg'
 import star_icon from './star_icon.svg'
-import left_arrow from './left_arrow.svg'
-import right_arrow from './right_arrow.svg'
-import header_img from './header_img.png'
 import brand_img from './brand_img.png'
 import project_img_1 from './project_img_1.jpg'
 import project_img_2 from './project_img_2.jpg'
 import project_img_3 from './project_img_3.jpg'
 import project_img_4 from './project_img_4.jpg'
-import project_img_5 from './project_img_5.jpg'
-import project_img_6 from './project_img_6.jpg'
-import profile_img_1 from './profile_img_1.png'
-import profile_img_2 from './profile_img_2.png'
-import profile_img_3 from './profile_img_3.png'
-// import Invextech from '../assets/Invextech.svg'
 import invextech from './invextech.svg'
 import mono from './mono.svg'
 import logo_invextech from './logo-invextech.png'
-import code_img from './code_img.jpg'
 import logo_preview from './logo_preview.png'
-import WhatsApp from './WhatsApp.jpg'
+import whatsApp from './WhatsApp.jpg'
 import development_img from './development_img.webp'
 import download from './download.jpeg'
 import images from './images.jpeg'
@@ -32,8 +22,8 @@ import images_3 from './images_3.png'
 import images_4 from './images_4.jpeg'
 import images_5 from './images_5.png'
 import images_6 from './images_6.jpeg'
-import CEO from './CEO.jpeg'
-import COO from './COO.jpeg'
+import ceo from './Ceo_img.jpeg'
+import coo from './Coo_img.jpeg'
 
 export const assets = {
     logo,
@@ -41,21 +31,16 @@ export const assets = {
     cross_icon,
     menu_icon,
     star_icon,
-    header_img,
     brand_img,
     project_img_1,
     project_img_2,
     project_img_3,
     project_img_4,
-    left_arrow,
-    right_arrow,
-    // Invextech,
     invextech,
     mono,
     logo_invextech,
-    code_img,
     logo_preview,
-    WhatsApp,
+    whatsApp,
     development_img,
     download,
     images,
@@ -65,7 +50,7 @@ export const assets = {
     images_4,
     images_5,
     images_6,
-    CEO,
-    COO
+    ceo,
+    coo,
 }
-export default assets
+export default assets ;
