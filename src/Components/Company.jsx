a11f70c8dcbd36dc0944734ae401a629979d4cf0
unknown <{huzaifazaifi25@gmail.com}>
2024-12-24 13:31:33 +0500

    change images path

--- src/Components/Company.jsx
+++ src/Components/Company.jsx
@@ -5,7 +5,7 @@ const Company = () => {
     {
       name: "Mr. Abu-Bakar Waqas",
       role: "CEO",
-      image: "https://www.linkedin.com/in/abubakar-waqas-a25182199/overlay/photo",
+      image: "/src/assets/Ceo_img.jpeg",
       bio: "A visionary leader driving innovation and growth at our company.",
       socials: {
         linkedin: "https://www.linkedin.com/in/abubakar-waqas-a25182199",
@@ -14,7 +14,7 @@ const Company = () => {
     {
       name: "Mr. Muhsan Sattar",
       role: "COO",
-      image: "https://www.linkedin.com/in/muhsan-sattar-b7b0801a4/overlay/photo",
+      image: "/src/assets/Coo_img.jpeg",
       bio: "An operational expert ensuring seamless execution of our strategies.",
       socials: {
         linkedin: "https://www.linkedin.com/in/muhsan-sattar-b7b0801a4",
--- src/assets/assets.js
+++ src/assets/assets.js
@@ -5,7 +5,7 @@ import menu_icon from '../assets/menu_icon.svg';
 import invextech from '../assets/invextech.svg';
 import mono from '../assets/mono.svg';
 import logo_preview from '../assets/logo_preview.png';
-import header from '../assets/header_img.jpeg';
+import header from '../src/assets/header_img.jpeg';
 import development_img from '../assets/development_img.webp';
 import download from '../assets/download.jpeg';
 import images from '../assets/images.jpeg';
@@ -15,8 +15,8 @@ import images_3 from '../assets/images_3.png';
 import images_4 from '../assets/images_4.jpeg';
 import images_5 from '../assets/images_5.png';
 import images_6 from '../assets/images_6.jpeg';
-import ceo from '../assets/Ceo_img.jpeg'; 
-import coo from '../assets/Coo_img.jpeg'; 
+import ceo from '..src/assets/Ceo_img.jpeg'; 
+import coo from '..src/assets/Coo_img.jpeg'; 
 
 export const assets = {
     logo,
