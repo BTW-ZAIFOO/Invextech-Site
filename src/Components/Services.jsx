d2987a3a45c4d8dc9ec4af0bc48f236e48de39a9
unknown <{huzaifazaifi25@gmail.com}>
2024-12-27 18:19:25 +0500

    added files and some functionality

--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -13,7 +13,7 @@ function Services() {
             </span>
           </h2>
           <h3 className="text-slate-800 font-bold text-center items-center mb-6 mt-6 text-3xl md:text-2xl">
-          Custom Software Development
+            Custom Software Development
           </h3>
         </div>
       </div>
@@ -44,7 +44,7 @@ function Services() {
       </div>
 
       {/* Mobile App Development Service */}
-      <div className="max-w-[1120px] mx-auto flex flex-col md:flex-row-reverse items-center justify-between mt-4 px-4 ">
+      <div className="max-w-[1120px] mx-auto flex flex-col md:flex-row-reverse items-center justify-between mt-4 px-4">
         <div className="w-full md:w-1/3 p-4">
           <img
             src={assets.images_2}
@@ -96,7 +96,7 @@ function Services() {
         <div className="w-full md:w-1/3 p-4">
           <img
             src={assets.images_4}
-            alt="Cloud Services"
+            alt="WordPress Solutions"
             className="rounded-lg shadow-md w-full h-auto"
           />
         </div>
@@ -105,12 +105,10 @@ function Services() {
             WordPress Solutions
           </h2>
           <p className="text-justify pt-5 text-base md:text-justify">
-            Unlock the potential of the cloud with our reliable and scalable
-            solutions. Whether you need cloud storage, powerful computing
-            resources, or app hosting, we ensure your systems are optimized for
-            top performance, high availability, and cost efficiency. Our team
-            helps you take full advantage of the cloud to support your business
-            growth and operational needs.
+            Unlock the potential of WordPress with our reliable and scalable
+            solutions. Whether you need tailored themes, powerful plugins, or app
+            hosting, we ensure your site is optimized for top performance, high
+            availability, and cost efficiency.
           </p>
         </div>
       </div>
@@ -131,10 +129,7 @@ function Services() {
           <p className="text-justify text-base pt-5 md:text-justify">
             Expand your business with advanced blockchain technology. We provide
             customized blockchain solutions that improve transparency,
-            strengthen security, and boost efficiency. From creating smart
-            contracts to building decentralized applications (dApps), we help
-            you harness the full potential of blockchain to meet your unique
-            business needs.
+            strengthen security, and boost efficiency.
           </p>
         </div>
       </div>
@@ -145,7 +140,7 @@ function Services() {
           <img
             src={assets.development_img}
             alt="SEO Solutions"
-            className="rounded-lg shadow-md w-full p-2 h-auto bg-white"
+            className="rounded-lg shadow-md w-full h-auto"
           />
         </div>
         <div className="w-full md:w-1/2 text-gray-800 px-4">
@@ -155,9 +150,7 @@ function Services() {
           <p className="text-justify text-base pt-5 md:text-justify">
             Boost your online presence with our customized SEO strategies. We
             handle everything from keyword research to on-page optimization,
-            ensuring your website ranks higher on search engines. Our approach
-            drives organic traffic to your site, enhances user engagement, and
-            increases conversion rates, helping your business grow effectively.
+            ensuring your website ranks higher on search engines.
           </p>
         </div>
       </div>
