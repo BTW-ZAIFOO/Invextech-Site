7fc1775022eccc24a81237a1797d81fab9ed436c
unknown <{huzaifazaifi25@gmail.com}>
2025-01-07 12:09:21 +0500

    added some functionality

--- src/Components/ChatBot/MessageInput.jsx
+++ src/Components/ChatBot/MessageInput.jsx
@@ -7,11 +7,11 @@ const MessageInput = ({ input, onInputChange, onSendMessage, onKeyDown }) => {
           onChange={onInputChange}
           onKeyDown={onKeyDown}
           placeholder="Type your message..."
-          className="flex-1 bg-transparent border-none focus:outline-none text-black placeholder-gray-700 py-2 px-3 rounded-xl transition-all duration-300 ease-in-out transform hover:scale-105"
+          className="flex-1 bg-transparent border-none focus:outline-none text-black placeholder-gray-700 py-2 px-3 rounded-xl transition-transform duration-300 hover:scale-105 sm:text-sm md:text-base"
         />
         <button
           onClick={onSendMessage}
-          className="ml-3 bg-gradient-to-r from-indigo-500 to-pink-500 text-white px-4 py-2 rounded-full shadow-lg hover:scale-105 transform transition hover:shadow-xl"
+          className="ml-3 bg-gradient-to-r from-indigo-500 to-pink-500 text-white px-3 py-2 rounded-full shadow-lg hover:scale-105 transform transition hover:shadow-xl sm:px-3 sm:py-1 md:px-4 md:py-2"
         >
           Send
         </button>
